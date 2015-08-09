7eda3040a2c3475146d071db77dd854b7daef529
monmonmon <ymdsmn@gmail.com>
2015-08-09 15:49:51 +0900

    add "locale" default url parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :set_locale
+
+  # 全リンクにlocale情報をセットする
+  def default_url_options(options = {})
+    { locale: I18n.locale }
+  end
+
+  # リンクの多言語化に対応する
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
   def full_screen_width
     @full_screen_width = true
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,33 @@
 Rails.application.routes.draw do
-  devise_for :users, path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-  }
+  scope "(:locale)", locale: /en|ja/ do
+    devise_for :users, skip: :omniauth_callbacks, path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+    }
+    match "/users/auth/:provider",
+      constraints: { provider: /google|facebook/ },
+      to: "devise/omniauth_callbacks#passthru",
+      as: :omniauth_authorize,
+      via: [:get, :post]
+    match "/users/auth/:action/callback",
+      constraints: { action: /google|facebook/ },
+      to: "devise/omniauth_callbacks#:action",
+      as: :omniauth_callback,
+      via: [:get, :post]
 
-  controller :welcome do
-    get 'hoe'
-    get 'about'
-    get 'contact'
-    get 'team'
+    controller :welcome do
+      get 'hoe'
+      get 'about'
+      get 'contact'
+      get 'team'
+    end
+
+    resources :bugs
+
+    resources :posts
+
+    resources :comments
+
+    root 'welcome#index'
   end
-
-  resources :bugs
-
-  resources :posts
-
-  resources :comments
-
-  root 'welcome#index'
 end
