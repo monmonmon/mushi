851169ca7a49b548861e46c97888bcb2d8eacdf8
monmonmon <ymdsmn@gmail.com>
2015-09-30 01:02:15 +0900

    strong parameter for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # 全リンクにlocale情報をセットする
   def default_url_options(options = {})
@@ -18,4 +19,11 @@ class ApplicationController < ActionController::Base
   def full_screen_width
     @full_screen_width = true
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+    end
 end
